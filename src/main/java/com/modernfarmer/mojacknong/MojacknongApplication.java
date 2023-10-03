d7a7e1081145c351e1265139b516a58f6f036b8a
MinchoGreenT <dldmstjq99@gmail.com>
2023-10-03 23:45:09 +0900

    initial commit

---
+++ src/main/java/com/modernfarmer/mojacknong/MojacknongApplication.java
@@ -0,0 +1,13 @@
+package com.modernfarmer.mojacknong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojacknongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MojacknongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modernfarmer/mojacknong/MojacknongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modernfarmer.mojacknong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojacknongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
